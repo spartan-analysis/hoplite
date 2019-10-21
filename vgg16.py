551ed962808624d5781d92096e6b6692e90998db
pry <pranoydll@gmail.com>
2019-10-21 10:55:51 -0500

    Averaged multiple test sparsities
    added output

--- vgg16.py
+++ vgg16.py
@@ -7,9 +7,7 @@ from sparsity import calculate_sparsity
 
 
 tiny_imgnet = "./tiny-imagenet-200/test/images/"
-img_path = tiny_imgnet + "test_0.JPEG"
-img = image.load_img(img_path, target_size=(224, 224))
-x = preprocess_input(np.expand_dims(image.img_to_array(img), axis=0))
+test_number = 10
 
 model = VGG16(
     include_top=True,
@@ -20,9 +18,28 @@ model = VGG16(
     classes=1000,
 )
 
-sparsities = calculate_sparsity(model, x)
+sparsities = []
+
+for i in range(test_number):
+    img_path = tiny_imgnet + "test_{}.JPEG".format(i)
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = preprocess_input(np.expand_dims(image.img_to_array(img), axis=0))
+    test_sparsities = calculate_sparsity(model, x)
+
+    index = 0
+    for j in test_sparsities:
+        if index < len(sparsities):
+            sparsities[index] = sparsities[index] + j
+        else:
+            sparsities.append(j)
+        index = index + 1
+
+sparsities = [x / test_number for x in sparsities]
 
 print(sparsities)
 
+fig = plt.figure()
 plt.bar(range(len(sparsities)), sparsities)
+plt.title("Sparsity by CONV Layer")
+fig.savefig("plot.png")
 plt.show()
