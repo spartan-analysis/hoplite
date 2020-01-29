fixed average sparsity bugs

hoplite.py
@@ -90,15 +90,11 @@ class LayerData:
         ).tolist()
 
 
-# example preprocess for vgg16 TODO improve this structure
-
-# TODO change the data gathering to count small values as zeroes as well
-# instead of just exact 0s
 class Hoplite:
     """Hoplite Sparsity Analyzer"""
 
     # preprocess is a function
-    def __init__(self, model, preprocess, output_filename):
+    def __init__(self, model, preprocess, output_filename, zero_sensitivity=0):
         self.model = model
         # relevant layers are conv and input
         self.layers = [
@@ -109,18 +105,29 @@ class Hoplite:
 
         self.input_layer_data = 0
         self.conv_layers_data = {}
+        self.zero_sensitivity = zero_sensitivity
 
-    @staticmethod
-    def compute_average_sparsity(output):
-        return output.size - np.count_nonzero(output)
+    def equals_zero(self, number):
+        return abs(number) <= self.zero_sensitivity
 
-    @staticmethod
-    def consec_1d(arr, hist):
+    def compute_average_sparsity(self, output):
+        # print(output.size)
+        # print(np.count_nonzero(output))
+        # return float(output.size - np.count_nonzero(output)) / float(output.size)
+        count = 0
+        for chan in output:
+            for col in chan:
+                for number in col:
+                    if self.equals_zero(number):
+                        count += 1
+        return float(count) / output.size
+
+    def consec_1d(self, arr, hist):
         all_nonzeroes = True
         count = 0
         for a in range(len(arr)):
             end = a == (len(arr) - 1)
-            if arr[a] == 0:
+            if self.equals_zero(arr[a]):
                 all_nonzeroes = False
                 count += 1
                 if end:
@@ -132,56 +139,51 @@ class Hoplite:
                 if end and all_nonzeroes:
                     hist[0] += 1
 
-    @staticmethod
-    def consec_row(output):
+    def consec_row(self, output):
         row_hist = [0] * (len(output[0][0]) + 1)
-        np.apply_along_axis(Hoplite.consec_1d, 2, output, row_hist)
+        np.apply_along_axis(self.consec_1d, 2, output, row_hist)
         return row_hist
 
-    @staticmethod
-    def consec_col(output):
+    def consec_col(self, output):
         col_hist = [0] * (len(output[0]) + 1)
-        np.apply_along_axis(Hoplite.consec_1d, 1, output, col_hist)
+        np.apply_along_axis(self.consec_1d, 1, output, col_hist)
         return col_hist
 
-    @staticmethod
-    def consec_chan(output):
-        print(np.shape(output))
+    def consec_chan(self, output):
         chan_hist = [0] * (len(output) + 1)
-        np.apply_along_axis(Hoplite.consec_1d, 0, output, chan_hist)
+        np.apply_along_axis(self.consec_1d, 0, output, chan_hist)
         return chan_hist
 
     @staticmethod
     def chunk_array(lst, n):
         for i in range(0, len(lst), n):
-            yield lst[i:i + n]
+            yield lst[i : i + n]
 
-    @staticmethod
-    def vec_1d(arr, vec_size, hist):
+    def vec_1d(self, arr, vec_size, hist):
         if len(arr) < vec_size:
             return
 
         chunks = Hoplite.chunk_array(arr, vec_size)
         for chunk in chunks:
-            zeroes = len(chunk) - np.count_nonzero(chunk)
+            zeroes = 0
+            for num in chunk:
+                if self.equals_zero(num):
+                    zeroes += 1
             hist[zeroes] += 1
 
-    @staticmethod
-    def vec_3d_row(output, vec_size):
+    def vec_3d_row(self, output, vec_size):
         vec_row_hist = [0] * (vec_size + 5)
-        np.apply_along_axis(Hoplite.vec_1d, 2, output, vec_size, vec_row_hist)
+        np.apply_along_axis(self.vec_1d, 2, output, vec_size, vec_row_hist)
         return vec_row_hist
 
-    @staticmethod
-    def vec_3d_col(output, vec_size):
+    def vec_3d_col(self, output, vec_size):
         vec_col_hist = [0] * (vec_size + 5)
-        np.apply_along_axis(Hoplite.vec_1d, 1, output, vec_size, vec_col_hist)
+        np.apply_along_axis(self.vec_1d, 1, output, vec_size, vec_col_hist)
         return vec_col_hist
 
-    @staticmethod
-    def vec_3d_chan(output, vec_size):
+    def vec_3d_chan(self, output, vec_size):
         vec_chan_hist = [0] * (vec_size + 5)
-        np.apply_along_axis(Hoplite.vec_1d, 0, output, vec_size, vec_chan_hist)
+        np.apply_along_axis(self.vec_1d, 0, output, vec_size, vec_chan_hist)
         return vec_chan_hist
 
     def analyze(self, filename):
@@ -199,15 +201,16 @@ class Hoplite:
                 self.input_layer_data = LayerData(
                     name, self.model.layers[0].output_shape[0][1:]
                 )
-
-                self.input_layer_data.average_sparsity = Hoplite.compute_average_sparsity(output)
+                self.input_layer_data.average_sparsity = self.compute_average_sparsity(
+                    output
+                )
 
             # for conv layers
             temp = LayerData(layer, self.model.get_layer(layer).output_shape[1:])
-            temp.average_sparsity = Hoplite.compute_average_sparsity(x)
-            temp.row_hist = Hoplite.consec_row(output)
-            temp.col_hist = Hoplite.consec_col(output)
-            temp.chan_hist = Hoplite.consec_chan(output)
+            temp.average_sparsity = self.compute_average_sparsity(output)
+            temp.row_hist = self.consec_row(output)
+            temp.col_hist = self.consec_col(output)
+            temp.chan_hist = self.consec_chan(output)
 
             temp.vec4_row_hist = self.vec_3d_row(output, 4)
             temp.vec4_col_hist = self.vec_3d_col(output, 4)
@@ -225,8 +228,8 @@ class Hoplite:
             temp.vec32_col_hist = self.vec_3d_col(output, 32)
             temp.vec32_chan_hist = self.vec_3d_chan(output, 32)
 
-            if 'input' not in layer:
-                #if self.conv_layers_data[layer] is None:
+            if "input" not in layer:
+                # if self.conv_layers_data[layer] is None:
                 if layer not in self.conv_layers_data:
                     self.conv_layers_data[layer] = temp
                 else: